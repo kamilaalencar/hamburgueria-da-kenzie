913d6cef675b49d6a0e9fa1aa45c9d379cc98656
Kamila Alencar <kamilaalencar1998@gmail.com.(none)>
2022-02-11 00:47:23 -0300

    feat: making API request

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [currentSale, setCurrentSale] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
+
+  useEffect(() => {
+    fetch("https://hamburgueria-kenzie-json-serve.herokuapp.com/products")
+      .then((response) => response.json())
+      .then((response) => setProducts(response.results));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
